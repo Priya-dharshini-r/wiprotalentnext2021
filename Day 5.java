396c64caa0118d3452a5890938b14818f891f3b3
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-05-29 18:13:08 +0530

    Update Day 5.java

--- Day 5.java
+++ Day 5.java
@@ -61,3 +61,66 @@ class Result {
 }
 
 public class Solution {
+
+  
+  //River records 
+  
+  import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'maxTrailing' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int maxTrailing(List<Integer> arr) {
+    // Write your code here
+    int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int array[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = s.nextInt();
+        }
+    int maxDiff = -1; 
+    
+    // Initialize max element from right side 
+    int maxRight = array[n-1]; 
+
+    for (int i = n-2; i >= 0; i--) 
+    { 
+        if (array[i] > maxRight) 
+            maxRight = array[i]; 
+        else
+        { 
+            int diff = maxRight - array[i]; 
+            if (diff > maxDiff) 
+            { 
+                maxDiff = diff; 
+            } 
+        } 
+    } 
+    return maxDiff; 
+    }
+
+}
+
+public class Solution {
