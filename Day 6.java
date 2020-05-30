619d67857245392ffa162a7b1a256c0bc18accee
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-05-30 14:14:55 +0530

    Update Day 6.java

--- Day 6.java
+++ Day 6.java
@@ -59,3 +59,53 @@ class Result {
 }
 
 public class Solution {
+
+    
+    //substring coin
+    
+    import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'fewestCoins' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING coins as parameter.
+     */
+
+    public static int fewestCoins(String coins) {
+    // Write your code here
+    int count[]=new int[limit];
+    for(int i=0;i<n;i++)
+    {
+
+        count[str.charAt(i)]++;
+    }
+    int max_distinct=0;
+    for(int i=0;i<limit;i++)
+    {
+        if(count[i]!=0)
+        {
+            max_distinct++;
+        }
+    }
+    return max_distinct;
+
+    }
+
+}
+
+public class Solution {
