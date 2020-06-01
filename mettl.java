b8e25f4f3464ed3fe4271e8ec1e7d0226b752b7d
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-06-01 21:12:57 +0530

    Update mettl.java

--- mettl.java
+++ mettl.java
@@ -26,3 +26,14 @@ else
 {
 return input1%10;
 }
+
+4.//sum of last digit of two number
+  
+  public static sum(int inpu1,input 2)
+{
+int result=0;
+  int a=input1%10;
+  int b=input2%10;
+  total=a+b;
+  return total;
+}
