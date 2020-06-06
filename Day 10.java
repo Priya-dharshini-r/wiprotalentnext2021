7259427c34a3ed9810f1680d387bb89abf2c7970
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-06-06 19:45:44 +0530

    Update Day 10.java

--- Day 10.java
+++ Day 10.java
@@ -33,3 +33,85 @@ class Result {
 
 }
 public class Solution {
+    
+    //
+    
+    import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+class Result {
+
+    /*
+     * Complete the 'getMinDeletions' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int getMinDeletions(String s) {
+    // Write your code here
+    if (left == 'a') {
+            if (right == 'b')
+                return 'c';
+            if (right == 'c')
+                return 'b';
+        }
+
+        if (left == 'b') {
+            if (right == 'a')
+                return 'c';
+            if (right == 'c')
+                return 'a';
+        }
+
+        if (left == 'c') {
+            if (right == 'a')
+                return 'b';
+            if (right == 'b')
+                return 'a';
+        }
+
+        return left;
+    }
+
+
+    public static int reduction(String a) {
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < a.length(); i++) {
+            char res = a.charAt(i);
+            while (!stack.empty()) {
+                char peek = stack.peek();
+                if (peek == res)
+                    break;
+                else {
+                    char popped = stack.pop();
+                    res = redux(res, popped);
+                }
+            }
+            stack.push(res);
+        }
+        return stack.size();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+
+        for (int i = 0; i < N; i++) {
+            String p = br.readLine();
+            int res = Solution.reduction(p);
+            System.out.println(res);
+        }
+
+    }
+    }
+
+public class Solution {
