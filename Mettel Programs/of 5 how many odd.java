da320e5c7c3ce58b0e806f7927626764135902c1
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-06-07 20:48:57 +0530

    Create of 5 how many odd.java

---
+++ Mettel Programs/of 5 how many odd.java
@@ -0,0 +1,19 @@
+ int c=0;
+
+  if(input1<0)input1=(-1)*input1;
+  if(input2<0)input1=(-1)*input1;
+  if(input3<0)input1=(-1)*input1;
+  if(input4<0)input1=(-1)*input1;
+  if(input5<0)input1=(-1)*input1;
+  if(input1%2!=0)
+  c++;
+  if(input2%2!=0)
+  c++;
+  if(input3%2!=0)
+  c++;
+  if(input4%2!=0)
+  c++;
+  if(input5%2!=0)
+  c++;
+
+return c++;
