2df775c8118986df5e947eddba979a57b91da9ff
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-06-07 23:03:02 +0530

    Create isPalindrome.java

---
+++ Mettel Programs/isPalindrome.java
@@ -0,0 +1,16 @@
+//Is palindrome
+
+input1=input1.toLowerCase();
+int i,flag=1;
+for(i=0i<input1.length()/2;i++)
+{
+if(input1.charAt(i)!=input1.charAt(input1.length()-i-1))
+{
+flag=0;
+break;
+}
+}
+if(flag==0)
+return 1;
+return 2;
+}
